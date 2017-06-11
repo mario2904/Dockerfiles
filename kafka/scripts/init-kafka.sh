43c4e7953a7c84348c9435253b3f013ce0f936a8
mario2904 <orbegoso2904@gmail.com>
2017-06-11 18:47:43 -0400

    Modify Kafka init script.

--- kafka/scripts/init-kafka.sh
+++ kafka/scripts/init-kafka.sh
@@ -4,4 +4,4 @@
 $KAFKA_HOME/bin/zookeeper-server-start.sh -daemon $KAFKA_HOME/config/zookeeper.properties
 
 # Start Kafka server as daemon
-$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
+$KAFKA_HOME/bin/kafka-server-start.sh -daemon $KAFKA_HOME/config/server.properties
